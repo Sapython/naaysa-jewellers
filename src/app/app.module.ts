18ca0274badb9cff5a8c9cc77450792ada16abbc
brahma <beherabrahmananada77@gmail.com>
2022-06-14 22:24:56 +0530

    initializing naaysa-jewellers ,added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './client/home/home.component';
+import { HeaderComponent } from './client/header/header.component';
+import { ClientRoutingModule } from './client/client-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClientRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
