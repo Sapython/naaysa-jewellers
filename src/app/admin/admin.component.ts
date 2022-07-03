9a1eb528e7a488290c85fd58c69433efbf3ebe70
Raghav <70934347+raggzy271@users.noreply.github.com>
2022-07-03 17:43:22 +0530

    Set up admin panel

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss'],
+})
+export class AdminComponent implements OnInit {
+  currentPanel: string = 'Panel';
+  breakpoint: number = 1000;
+  largeScreen: boolean = window.innerWidth > this.breakpoint;
+  showSidebar: boolean = false;
+
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationEnd) {
+        // Update header heading
+        const url = this.router.url;
+        const urlArr = url.split('/');
+        const panel = urlArr[urlArr.length - 1];
+        const panelWords = panel.split('-');
+        panelWords.forEach((word, index) => {
+          panelWords[index] = word.charAt(0).toUpperCase() + word.substring(1);
+        });
+        this.currentPanel = panelWords.join(' ');
+      }
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onWindowResize() {
+    this.largeScreen = window.innerWidth > this.breakpoint;
+    document.documentElement.style.setProperty(
+      '--overlay-width',
+      !this.largeScreen && this.showSidebar ? '100%' : '0%'
+    );
+  }
+
+  toggleSidebar() {
+    this.showSidebar = !this.showSidebar;
+    document.documentElement.style.setProperty(
+      '--overlay-width',
+      !this.largeScreen && this.showSidebar ? '100%' : '0%'
+    );
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
       import('./client/client.module').then((m) => m.ClientModule),
   },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
 ];
 
 @NgModule({
