2aa9ce3762588befebd5b0da07938bd24ecc386b
brahma <beherabrahmananada77@gmail.com>
2022-07-10 20:47:45 +0530

    update header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 declare var UIkit:any;
-
-
+import { HostListener } from '@angular/core';
+@HostListener('window:scroll', ['$event'])
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  toggleSearch: boolean = false; 
   constructor() { }
-
   ngOnInit(): void {
     var element = document.getElementById('hover');
     console.log(element);
     console.log(UIkit.dropdown(element))
     UIkit.dropdown(element).delayHide = 0;
+    
+    
   }
-
+  
+  onWindowScroll() {
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('scroll');
+      console.log('add');
+    } else {
+      element.classList.remove('scroll');
+      console.log('remove');
+    }
+  }
 }
