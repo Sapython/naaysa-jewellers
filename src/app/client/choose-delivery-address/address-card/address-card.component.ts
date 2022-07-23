f27a76fb680d147150c8cc1ac951bdfef4925e39
vishnusharma0078 <88197254+vishnusharma0078@users.noreply.github.com>
2022-07-23 12:33:11 +0530

    fixed choose delivery address page

--- src/app/client/choose-delivery-address/address-card/address-card.component.ts
+++ src/app/client/choose-delivery-address/address-card/address-card.component.ts
@@ -7,11 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AddressCardComponent implements OnInit {
 
-  @Input() name:String="";
-  @Input() last_name:String="";
-  @Input() address_type:String="";
+  @Input() name:String="JOHN";
+  @Input() last_name:String="DOE";
+  @Input() address_type:String="Work";
   @Input() number:String="";
-  @Input() address:String="";
+  @Input() address:String="10880 Malibu Point, 90265 CA 90265, USA";
+  @Input() phone:String="+31 525-551-555";
 
   constructor() { }
 
