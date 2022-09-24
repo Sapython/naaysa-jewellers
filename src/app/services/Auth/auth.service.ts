32a4950d5693b1305d72c12fa448092239c910b7
Rohan509 <koushik.rohan509@gmail.com>
2022-09-24 16:14:49 +0530

    Push

--- src/app/services/Auth/auth.service.ts
+++ src/app/services/Auth/auth.service.ts
@@ -1,18 +1,26 @@
+import { Address, ExtraLoginEmailInfo } from './../../structures/user.structure';
 import { Injectable } from '@angular/core';
-import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, UserCredential } from '@angular/fire/auth';
-import { addDoc, collection, doc, Firestore, getDoc, getDocs } from '@angular/fire/firestore';
+import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, User, UserCredential } from '@angular/fire/auth';
+import { addDoc, collection, doc, DocumentReference, Firestore, getDoc, getDocs, setDoc, updateDoc } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { UserData } from 'src/app/structures/user.structure';
 import { DataproviderService } from '../dataprovider.service';
 import { urls } from '../urls';
+import { async, EMPTY, Observable, Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  user: any;
+
+  public userAvailable: Subject<any> = new Subject()
   public userId: any;
   public userIsLoggedIn: boolean;
   public newUser: boolean = false
+  userDoc: DocumentReference;
+  cartItem: DocumentReference;
+
 
   constructor(private fs: Firestore, private auth: Auth, private router: Router, public dataprovider: DataproviderService) {
     if (auth) {
@@ -25,6 +33,7 @@ export class AuthService {
           this.userIsLoggedIn = true;
           this.userId = user.uid
           this.dataprovider.userData = user;
+          this.userAvailable.next(user)
         }
         else {
           this.userIsLoggedIn = false;
@@ -35,9 +44,17 @@ export class AuthService {
     }
   }
 
+  public readonly user: Observable<User | null> = EMPTY;
 
 
-  public emailLogin(email: any, password: any) {
+  public get getUser(): Observable<User | null> {
+    console.log(this.user)
+    return this.user;
+
+  }
+
+
+  public loginWithEmailPassword(email: any, password: any) {
     return signInWithEmailAndPassword(this.auth, email, password).then((credentials: UserCredential) => {
       console.log(credentials)
 
@@ -46,9 +63,12 @@ export class AuthService {
     })
   }
 
-  emailSignUp(email: any, password: any) {
-    return createUserWithEmailAndPassword(this.auth, email, password).then( (credentials: UserCredential) => {
-     
+  public signUpWithEmailAndPassword(email: any, password: any) {
+    let data = createUserWithEmailAndPassword(this.auth, email, password).then(async (credentials: UserCredential) => {
+      await this.setEmailUserData(credentials.user, {
+        phoneNumber: '', photoURL: '',
+        displayName: ''
+      })
       this.newUser = true;
 
     }).catch((err) => {
@@ -57,25 +77,146 @@ export class AuthService {
     })
   }
 
+
+  public async setEmailUserData(user: User, userData: ExtraLoginEmailInfo) {
+
+    let data: UserData = {
+      userId: user.uid || '',
+      email: user.email || '',
+      displayName: userData.displayName || '',
+      photoURL: userData.photoURL || this.getRandomImage(),
+      phoneNumber: userData.phoneNumber || '',
+      emailVerified: true,
+      firstLogin: false,
+      access: { access: 'user' },
+      orders: [],
+      totalOrders: 0,
+      totalCashback: 0,
+      wishlist: [],
+      cart: [],
+      address: {
+        country: '',
+        region: '',
+        pincode: 0,
+        FlatAddress: '',
+        houseNo: 0,
+        Landmark: '',
+        type: 'company',
+        city: '',
+        state: ''
+      },
+      dateOfBirth: '',
+      recommendation: []
+    }
+    this.userDoc = doc(this.fs, 'users/' + user.uid);
+    await setDoc(this.userDoc, data).then(() => {
+      alert('userDataSet')
+    });
+
+    this.router.navigate(['/all-products'])
+  }
+
+  getRandomImage(): string {
+    return 'https://avatars.dicebear.com/api/gridy/' + (Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)) + '.svg';
+  }
+
+
+
   // user collection
   public addUser(data: any) {
     return addDoc(collection(this.fs, urls.users), data);
   }
 
-  public getUser(USER_ID: any) {
+  public getDbUser(USER_ID: any) {
     const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
-    return getDoc(doc(this.fs, userIDUrl)).then((res) => { }).catch((err) => { console.log(err) });
+    return getDoc(doc(this.fs, userIDUrl));
   }
 
   getAllUsers() {
     return getDocs(collection(this.fs, '/AllUsers'))
   }
 
+  // edit User
+
+  updateUser(USER_ID: any, data: any) {
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+    return updateDoc(doc(this.fs, userIDUrl), data).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
+
+  // cart
+
+
+
+  // wishlist
+
+  userWishlist(USER_ID: any, data: any) {
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+
+    return addDoc(collection(this.fs, userIDUrl + urls.wishlist), data).then((res) => {
+
+    });
+  }
+
+  getUserWishlist(USER_ID: any) {
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+
+    return getDocs(collection(this.fs, userIDUrl + urls.wishlist))
+  }
+
+  // Cart
+
+  getUserCartList(USER_ID: any) {
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+
+    return getDocs(collection(this.fs, userIDUrl + urls.cart))
+  }
+
+  userCart(USER_ID: any, data: any) {
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+    data['productId'] = data.id
+    return addDoc(collection(this.fs, userIDUrl + urls.cart), data).then(async (res) => {
+
+    });
+  }
+
+  deleteUserCart(USER_ID: any, cartId: any){
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+    const cartIDUrl = urls.cartId.replace('{USER_ID}', USER_ID);
+  }
+
+  // Orders
+
+
+  userOrders(USER_ID: any, data: any) {
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+    return addDoc(collection(this.fs, userIDUrl + urls.orders), data).then(async (res) => {
+
+    });
+  }
+
+  // Add Delivery addresss
+
+  addDeliveryAddress(USER_ID: any, data: any) {
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+    return addDoc(collection(this.fs, userIDUrl + urls.deliveryAddress), data).then(async (res) => {
+
+    });
+  }
+
+  getDeliveryAddress(USER_ID: any) {
+    const userIDUrl = urls.user.replace('{USER_ID}', USER_ID);
+    return getDocs(collection(this.fs, userIDUrl + urls.deliveryAddress)).then()
+  }
+
   public async logout() {
-   
-    this.router.navigate(['/signup']);
+    alert('Logged Out!');
+
     this.dataprovider.userData = false;
-    return await signOut(this.user);
+    return await signOut(this.auth);
   }
 
 }
