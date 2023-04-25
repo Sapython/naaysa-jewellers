e5b96fd9cd7c867a3906258088a5929286179368
Reeva Singh <92727531+RE378@users.noreply.github.com>
2023-04-25 15:05:44 +0530

    Cart Page

--- src/app/client/cart/cart.component.ts
+++ src/app/client/cart/cart.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-  
+
   @Input() delivery_Date:string = '20 April 2022'
   @Input() disc_percentage:string = '30%'
   @Input() subtotal:string = '₹ 1,34,507'
@@ -23,11 +23,37 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     let cart = localStorage.getItem('cart')
-    if(cart){
-      this.cartItems = JSON.parse(cart)
-    } else {
-      this.cartItems = []
-    }
+    // if(cart){
+    //   this.cartItems = JSON.parse(cart)
+    // } else {
+    //   this.cartItems = []
+    // }
+    this.cartItems = [{
+    id:1,
+    name:"Ripple Cluster Daimond Stud",
+    no:"JE032328-WGP900",
+    src:"../../../assets/CART/BISJ0005R08-POSTER-4597.jpg.png",
+    price: "14,174",
+    stock: "Only 1 piece left",
+    quantity : "1",
+    date:"20 April 2022",
+    save:"₹2,350",
+    count: "1",
+    totalprice:"₹ 10,000",
+  },{
+    id:2,
+    name:"Ripple Cluster Daimond Stud",
+    no:"JE032328-WGP900",
+    src:"../../../assets/CART/BISJ0005R08-POSTER-4597.jpg.png",
+    price: "14,174",
+    stock: "Only 1 piece left",
+    date:"20 April 2022",
+    save:"₹ 2,350",
+    count: "1",
+    quantity : "1",
+    totalprice:"₹ 10,000",
+  }]
+  console.log(this.cartItems)
   }
 
 }
