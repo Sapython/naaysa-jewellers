a028a572a9810df05f96444f0d1853413e8657b8
Reeva Singh <92727531+RE378@users.noreply.github.com>
2023-05-05 13:57:31 +0530

    pincode component created

--- src/app/client/cart/cart.module.ts
+++ src/app/client/cart/cart.module.ts
@@ -6,11 +6,13 @@ import { CartComponent } from './cart.component';
 import { ComponentsModule } from '../../components/components.module';
 import { CartAddedItemComponent } from './cart-added-item/cart-added-item.component';
 import { WidgetsModule } from 'src/app/widgets/widgets.module';
+import { PincodeComponent } from './pincode/pincode.component';
 
 @NgModule({
   declarations: [
     CartComponent,
     CartAddedItemComponent,
+    PincodeComponent,
   ],
   imports: [
     CommonModule,
