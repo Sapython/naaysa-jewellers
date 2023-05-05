532ce11c47b0b5d87a382e5825e0191aa7dd42a0
Sayali-dhamane <129577517+Sayali-dhamane@users.noreply.github.com>
2023-05-05 17:29:55 +0530

    payment confirmation pop-up completed
    payment confirmation pop-up completed

--- src/app/client/home/home.component.ts
+++ src/app/client/home/home.component.ts
@@ -89,6 +89,70 @@ export class HomeComponent implements OnInit {
 
   ]
 
+  products: any[] = [
+    {
+      name: 'Royal Pink Diamond Ring',
+      price: 14325,
+      category: 'ring',
+      liked: true,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, laboriosam rem eum tenetur quidem quaerat',
+      tag: 'New In',
+      metalPurityTypes: [
+        {
+          purity: '18K',
+        },
+        {
+          purity: '22K',
+        },
+        {
+          purity: '24K',
+        },
+      ],
+      diamondTypes: [
+        {
+          name: '22 C',
+        },
+        {
+          name: '24 C',
+        },
+        {
+          name: '26 C',
+        },
+      ],
+      sizeTypes: [
+        {
+          size: '18',
+        },
+        {
+          size: '20',
+        },
+        {
+          size: '22',
+        },
+      ],
+      images: [
+        'assets/topBarImages/earings/ear (1).png',
+        'assets/topBarImages/earings/ear (6).png',
+        'assets/topBarImages/earings/ear (3).png',
+        'assets/topBarImages/earings/ear (4).png',
+        'assets/topBarImages/earings/ear (5).png',
+      ],
+      certifications: [
+        {
+          name: 'bis',
+          image: 'assets/badges/badge (1).png'
+        },
+        {
+          name: 'bis',
+          image: 'assets/badges/badge (2).png'
+        },
+        {
+          name: 'bis',
+          image: 'assets/badges/badge (3).png'
+        },
+      ]
+    }
+  ]
 
 
 
